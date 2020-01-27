2c098e45928cfb247d8ffbbbcb9ba2fe5b669732
Eva Larumbe <evalarumbe@gmail.com>
2020-01-27 22:47:07 +0100

    Add starter example

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 (() => {
-    
+    const form = document.querySelector('form');
+
+    const setColor = function(event) {
+        event.preventDefault();
+
+        const textField = document.querySelector('#color');
+        const square = document.querySelector('#square');
+        
+        square.style.backgroundColor = textField.value;
+    };
+
+    form.addEventListener('submit', setColor, false);
 })();
