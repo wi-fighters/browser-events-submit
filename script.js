b825772a7e8c7fbbef59873c151793bd2c675dcf
NAQVI <nomanalam7214@gmail.com>
2020-01-29 23:17:34 +0100

    .

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 (() => {
-    // store a reference to the event target/s
-    const form = document.querySelector('form');
 
-    // define the listener/s
-    const setColor = function(event) {
-        event.preventDefault();
-
-        const textField = document.querySelector('#color');
-        const square = document.querySelector('#square');
-        
-        square.style.backgroundColor = textField.value;
-    };
-
-    // attach the listeners to the event target/s
-    form.addEventListener('submit', setColor, false);
 })();
